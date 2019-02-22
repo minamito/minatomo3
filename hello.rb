ae64f3283d9f5e0b8bdef522cdc9d6993d92430a
南智哉 <techboostminami@gmail.com>
2019-02-22 14:34:23 +0900

    first commit

---
+++ hello.rb
@@ -0,0 +1,33 @@
+puts "始めてのRuby"
+puts "Ruby" + "始めました"
+
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+
+array = [1,2,3,4]
+
+num_array = array
+
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+
+p_info ={name: "南智哉", seinengappi: "1995/12/19", ketuekigata: "A"}
+p p_info[:name]
+p p_info[:seinengappi]
+p p_info[:ketuekigata]
+
+def plus_ruby(name)
+ p name + "ruby"
+end
+
+plus_ruby("minami")
+
+def plus_one(num)
+ p num + 1
+end
+
+plus_one(10)
